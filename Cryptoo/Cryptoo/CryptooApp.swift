69ca432a8594da682dbfb4758bdbc78ec0e8e30c
Andrey Prez <aw06.1994@yandex.ru>
2023-04-16 01:13:18 +0300

    Project added

---
+++ Cryptoo/Cryptoo/CryptooApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptooApp.swift
+//  Cryptoo
+//
+//  Created by Андрей  on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
