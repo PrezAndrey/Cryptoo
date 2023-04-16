6d911c2bebedd263d1fda232e9cdd7f6ab26c57a
Andrey Prez <aw06.1994@yandex.ru>
2023-04-17 00:43:00 +0300

    Creating homeView and a navigation header

--- Cryptoo/Cryptoo/CryptooApp.swift
+++ Cryptoo/Cryptoo/CryptooApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct CryptooApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
