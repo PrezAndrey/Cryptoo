0e365041f6f703f1cc9325e634e792b829360541
Andrey Prez <aw06.1994@yandex.ru>
2023-04-22 02:07:15 +0300

    Design a row to display coins in a List & extention for Double

---
+++ Cryptoo/Cryptoo/Core/Home/View/CoinRowView.swift
@@ -0,0 +1,80 @@
+//
+//  CoinRowView.swift
+//  Cryptoo
+//
+//  Created by Андрей  on 19.04.2023.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin: Coin
+    var showHoldingsColumn: Bool
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            leftColumn
+            Spacer()
+            centerColumn
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text("\(coin.symbol.uppercased())")
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        HStack(spacing: 0) {
+            if showHoldingsColumn {
+                VStack(alignment: .trailing) {
+                    Text(coin.currentHoldingsValue.asCurrencyWith6Decimals())
+                        .bold()
+                    Text((coin.currentHoldings ?? 0).asNumberString())
+                }
+                .foregroundColor(Color.theme.accent)
+            }
+        }
+    }
+    
+    private var rightColumn: some View {
+        HStack(spacing: 0) {
+            VStack(alignment: .trailing) {
+                Text(coin.currentPrice.asCurrencyWith2Decimals())
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                Text(coin.priceChangePercentage24H?.asPersentString() ?? "")
+                    .foregroundColor((coin.priceChangePercentage24H ?? 0) >= 0 ? Color.theme.green : Color.theme.red)
+            }
+            .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+        }
+    }
+}
